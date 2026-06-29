0bfdd5f0a4db05dd4fdddf0cf94481f5202289f7
Krish Chauhan <krish16chauhan@gmail.com>
2026-06-30 00:09:38 +0530

    Time: 8 ms (22.21%), Space: 18.3 MB (56.2%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        vector<vector<int>>adj(n);
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        vector<int>indeg(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<adj[i].size();j++)
+            {
+                indeg[adj[i][j]]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(!indeg[i])
+            {
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        int l=0;
+        while(!q.empty())
+        {
+            int first=q.front();
+            q.pop();
+            ans.push_back(first);
+            for(int i=0;i<adj[first].size();i++)
+            {
+                indeg[adj[first][i]]--;
+                if(indeg[adj[first][i]]==0)
+                {
+                    q.push(adj[first][i]);
+                }
+            }
+        }
+        if(ans.size()!=n)
+        return {};
+        return ans;
+    }
+};
