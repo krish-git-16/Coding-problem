bd9c767624a67dbce0dbcfad0b9afe0f566f76d8
Krish Chauhan <krishchauhan_se24b02_029@dtu.ac.in>
2026-07-17 21:48:17 +0530

    Time: 4 ms (81.3%), Space: 13.2 MB (59.86%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(string &w1,string &w2,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i<0)
-        {
-            return j+1;
-        }
-        if(j<0)
-        {
-            return i+1;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(w1[i]==w2[j])
-        {
-            return dp[i][j]=solve(w1,w2,i-1,j-1,dp);
-        }
-        return dp[i][j]=min(1+solve(w1,w2,i-1,j-1,dp),min(1+solve(w1,w2,i,j-1,dp),1+solve(w1,w2,i-1,j,dp)));
-    }
+    // int solve(string &w1,string &w2,int i,int j,vector<vector<int>>&dp)
+    // {
+    //     if(i<0)
+    //     {
+    //         return j+1;
+    //     }
+    //     if(j<0)
+    //     {
+    //         return i+1;
+    //     }
+    //     if(dp[i][j]!=-1)
+    //     {
+    //         return dp[i][j];
+    //     }
+    //     if(w1[i]==w2[j])
+    //     {
+    //         return dp[i][j]=solve(w1,w2,i-1,j-1,dp);
+    //     }
+    //     return dp[i][j]=min(1+solve(w1,w2,i-1,j-1,dp),min(1+solve(w1,w2,i,j-1,dp),1+solve(w1,w2,i-1,j,dp)));
+    // }
     int minDistance(string word1, string word2) {
         int n=word1.size(),m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
