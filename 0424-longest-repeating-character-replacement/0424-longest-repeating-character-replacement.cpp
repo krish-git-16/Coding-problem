18e5f7d07b82a620a83885f07d1d427f392ad50b
Krish Chauhan <krishchauhan_se24b02_029@dtu.ac.in>
2026-07-26 01:21:23 +0530

    Time: 18 ms (13.81%), Space: 10.8 MB (71.59%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int f=0,l=0,n=s.size(),mf=0,ans=0;
+        vector<int>alpha(26,0);
+        while(l<n)
+        {
+            alpha[s[l]-'A']++;
+            mf=*max_element(alpha.begin(),alpha.end());
+            while(l-f+1-mf>k)
+            {
+                alpha[s[f]-'A']--;
+                mf=*max_element(alpha.begin(),alpha.end());
+                f++;
+            }
+            ans=max(ans,l-f+1);
+            l++;
+        }
+        return ans;
+    }
+};
