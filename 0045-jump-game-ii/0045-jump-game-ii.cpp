39ce383c9a2540ba41724fd2d5f201a4f56c922e
Krish Chauhan <krishchauhan_se24b02_029@dtu.ac.in>
2026-08-22 14:04:58 +0530

    Time: 0 ms (100%), Space: 20.6 MB (54.32%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n =nums.size();
+        int l=0,r=0;
+        int jumps=0;
+        if(n==1)
+        return 0;
+        while(r<n-1)
+        {
+            int maxi=1;
+            for(int i=l;i<=r;i++)
+            {
+               maxi=max(maxi,i+nums[i]);
+            }
+            l=r+1;
+            r=maxi;
+            jumps++;
+        }
+        return jumps;
+    }
+};
