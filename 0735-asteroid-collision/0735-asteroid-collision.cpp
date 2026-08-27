2ce8f4e4c61ceb13ff2a7e51663b958fd9a7d898
Krish Chauhan <krishchauhan_se24b02_029@dtu.ac.in>
2026-08-27 19:12:02 +0530

    Time: 1 ms (38.88%), Space: 22.3 MB (18.27%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st;
+        int n=asteroids.size();
+        for(int i=0;i<n;i++)
+        {
+            if(asteroids[i]<0)
+            {
+                while(!st.empty()&&asteroids[st.top()]>0&&asteroids[st.top()]<abs(asteroids[i]))
+                {
+                    st.pop();
+                }
+                if(st.empty()||asteroids[st.top()]<0)
+                {
+                    st.push(i);
+                }
+                if(asteroids[st.top()]==abs(asteroids[i]))
+                st.pop();
+            }    
+            else
+            st.push(i);
+        }
+        vector<int>ans;
+        while(!st.empty())
+        {
+            ans.push_back(asteroids[st.top()]);
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
